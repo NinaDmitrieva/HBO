76f0f89b66ab0151deafa1bb0dc6234c01bfe5b5
Nina Dmitrieva <nina.dmitrieva.spb@yandex.ru>
2023-06-16 15:40:04 +0300

    feat: add slider

--- HBO/js/index.js
+++ HBO/js/index.js
@@ -1,5 +1,6 @@
 import { data as movieList } from './data.js';
 
+const createBTN = document.querySelector('.checkbox__btn-submit')
 const burgerBTN = document.querySelector('.header__burger')
 const menuList = document.querySelector('.header-menu')
 const signinBTN = document.querySelector('.signin__btn')
@@ -9,51 +10,53 @@ const modalBtn = document.querySelector('.modal__btn');
 const showsContainer = document.querySelector('.showsContainer');
 const moviesContainer = document.querySelector('.moviesContainer');
 const cartoonsContainer = document.querySelector('.cartoonsContainer');
+const sliderItemImg = document.querySelector('.slider-img');
+const sliderItemTitle = document.querySelector('.slider-box__logo');
 
 
 const toggleBurgerMenu = () => {
-    menuList.classList.toggle('hide')
-    signinBTN.classList.toggle('hide')
+  menuList.classList.toggle('hide')
+  signinBTN.classList.toggle('hide')
 }
 
 const closeBurgerMenu = () => {
-    menuList.classList.add('hide')
-    signinBTN.classList.add('hide')
+  menuList.classList.add('hide')
+  signinBTN.classList.add('hide')
 }
 
 const handleSign = () => {
-    closeBurgerMenu();
-    openModal()
-    overlay.style.filter = 'blur(4px)';
+  closeBurgerMenu();
+  openModal()
+  overlay.classList.add('blur')
 }
 
 const openModal = () => {
-    modal.style.opacity = '1';
-    document.addEventListener('keyup', handleEscClose);
+  modal.classList.add('activ')
+  document.addEventListener('keyup', handleEscClose);
 
 }
 const closeModal = () => {
-    modal.style.opacity = '0';
-    overlay.style.filter = 'blur(0)';
-    toggleBurgerMenu()
-    document.removeEventListener('keyup', handleEscClose);
+  modal.classList.remove('activ')
+  overlay.classList.remove('blur')
+  toggleBurgerMenu()
+  document.removeEventListener('keyup', handleEscClose);
 }
 
 const handleEscClose = (e) => {
-    if (e.code === 'Escape') {
-        closeModal()
-    }
+  if (e.code === 'Escape') {
+    closeModal()
+  }
 }
 
 const getMovieShow = (arr, type) => {
-    const movieShow = arr.filter(show => show.type === type)
-    return movieShow
+  const movieShow = arr.filter(show => show.type === type)
+  return movieShow
 }
 
 const showMovieShow = (arr) => {
 
-    arr.forEach(show => {
-        const showItem = `
+  arr.forEach(show => {
+    const showItem = `
          <div class="movie-item">
                     <img
                       class="movie-item__img"
@@ -88,31 +91,37 @@ const showMovieShow = (arr) => {
                     </div>
                   </div>
         `
-        if (show.type === 'movie') {
-            moviesContainer.insertAdjacentHTML('beforeend', showItem)
-        } else if (show.type === 'show') {
-            showsContainer.insertAdjacentHTML('beforeend', showItem)
-        } else if (show.type === 'cartoon') {
-            cartoonsContainer.insertAdjacentHTML('beforeend', showItem)
-        }
-    });
+    if (show.type === 'movie') {
+      moviesContainer.insertAdjacentHTML('beforeend', showItem)
+    } else if (show.type === 'show') {
+      showsContainer.insertAdjacentHTML('beforeend', showItem)
+    } else if (show.type === 'cartoon') {
+      cartoonsContainer.insertAdjacentHTML('beforeend', showItem)
+    }
+  });
 }
 showMovieShow(getMovieShow(movieList, 'movie'))
 showMovieShow(getMovieShow(movieList, 'show'))
 showMovieShow(getMovieShow(movieList, 'cartoon'))
 
+const slider = () => {
+ 
+}
+setInterval(() => {
 
+}, 5000)
 
+// sliderItemImg.style.backgroundImage = 'url(./img/movie1.png)'
+// sliderItemTitle.src = './img/movie1logo.png'
 
-function get_scroll(a) { //что по скроллу
-    var d = document,
-        b = d.body,
-        e = d.documentElement,
-        c = "client" + a;
-    a = "scroll" + a;
-    return /CSS/.test(d.compatMode) ? (e[c] < e[a]) : (b[c] < b[a])
-};
-console.log(get_scroll('Width'))
+sliderItemImg.style.backgroundImage = 'url(./img/movie2.png)'
+sliderItemTitle.src = './img/movie2logo.png'
+
+//sliderItemImg.style.backgroundImage = 'url(./img/movie3.png)'
+//sliderItemTitle.src = './img/sliderLogo.png'
+
+// console.log(sliderItemImg.style.backgroundImage)
+
 
 overlay.addEventListener('click', closeModal)
 burgerBTN.addEventListener('click', toggleBurgerMenu);
