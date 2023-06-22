fbea304dcf7400133c4149dc24db97f77ffc29d7
Nina Dmitrieva <nina.dmitrieva.spb@yandex.ru>
2023-06-23 01:06:23 +0300

    fix: err styles

--- HBO/js/index.js
+++ HBO/js/index.js
@@ -1,14 +1,13 @@
 import { data as movieList } from './data.js';
 
+const faqItem = document.querySelectorAll('.faq-container__item');
+const links = document.querySelectorAll('.header-menu__item');
 const burgerBTN = document.querySelector('.header__burger')
 const menuList = document.querySelector('.header-menu')
 const signinBTN = document.querySelector('.signin__btn')
 const overlay = document.querySelector('.main');
 const modal = document.querySelector('.modal');
 const modalBtn = document.querySelector('.modal__btn');
-const showsContainer = document.querySelector('.showsContainer');
-const moviesContainer = document.querySelector('.moviesContainer');
-const cartoonsContainer = document.querySelector('.cartoonsContainer');
 const sliderItemImg = document.querySelector('.slider-img');
 const sliderItemTitle = document.querySelector('.slider-box__logo');
 const sliderList = [
@@ -25,55 +24,51 @@ const sliderList = [
     titleSrc: './img/sliderLogo.png'
   },
 ]
+const whaеScreen = () => {
+  if (window.matchMedia(`(min-width: 769px)`).matches) {
+    menuList.classList.remove('hide')
+    signinBTN.classList.remove('hide')
+  }
+}
 
 const toggleBurgerMenu = () => {
   menuList.classList.toggle('hide')
   signinBTN.classList.toggle('hide')
 }
 
-const closeBurgerMenu = () => {
-  menuList.classList.add('hide')
-  signinBTN.classList.add('hide')
+const toggleModal = () => {
+  modal.classList.toggle('activ')
+  overlay.classList.toggle('blur')
+
+  if (modal.classList.contains('activ')) {
+    document.addEventListener('keyup', handleEscClose);
+  }
+  else {
+    document.removeEventListener('keyup', handleEscClose);
+  }
+  window.addEventListener("resize", whaеScreen());
 }
 
-const handleSign = () => {
-  closeBurgerMenu();
-  openModal()
-  overlay.classList.add('blur')
-}
-
-const openModal = () => {
-  modal.classList.add('activ')
-  document.addEventListener('keyup', handleEscClose);
-
-}
-const closeModal = () => {
-  modal.classList.remove('activ')
-  overlay.classList.remove('blur')
-  toggleBurgerMenu()
-  document.removeEventListener('keyup', handleEscClose);
+const handleSignForm = () => {
+  toggleBurgerMenu();
+  toggleModal()
 }
 
 const handleEscClose = (e) => {
-  if (e.code === 'Escape') {
-    closeModal()
+  if (e.code === 'Escape' && modal.classList.contains('activ')) {
+    toggleModal()
   }
 }
 
-const getMovieShow = (arr, type) => {
-  const movieShow = arr.filter(show => show.type === type)
-  return movieShow
-}
+const getFilterTtems = (arr, type) => arr.filter((movie) => movie.type === type)
 
-const showMovieShow = (arr) => {
-
-  arr.forEach(show => {
-    const showItem = `
+const itemTemplate = (item) => {
+  return `
          <div class="movie-item">
                     <img
                       class="movie-item__img"
-                      src=${show.image}
-                      alt=${show.title}
+                      src=${item.image}
+                      alt=${item.title}
                     />
 
                     <div class="movie-item__info">
@@ -83,38 +78,43 @@ const showMovieShow = (arr) => {
                             class="rating-icon"
                             src="../HBO/img/icon/starICON.png"
                           />
-                          <p class="rating-text">${show.rating}</p>
+                          <p class="rating-text">${item.rating}</p>
                         </div>
 
-                        <p class="movie-item__info-header__year">${show.year}</p>
+                        <p class="movie-item__info-header__year">${item.year}</p>
                       </div>
 
-                      <h2 class="movie-item__info-name">${show.title}</h2>
-                      <p class="movie-item__info-description">${show.description}</p>
+                      <h2 class="movie-item__info-name">${item.title}</h2>
+                      <p class="movie-item__info-description">${item.description}</p>
                       <div class="watch-wrapper flex">
                         <img
                           class="watch-wrapper__playIcon"
                           src="../HBO/img/icon/playICON.png"
                         />
-                        <a href=${show.trailer} target="_blank" class="watch-wrapper__text"
+                        <a href=${item.trailer} target="_blank" class="watch-wrapper__text"
                           >WATCH</a
                         >
                       </div>
                     </div>
                   </div>
         `
-    if (show.type === 'movie') {
-      moviesContainer.insertAdjacentHTML('beforeend', showItem)
-    } else if (show.type === 'show') {
-      showsContainer.insertAdjacentHTML('beforeend', showItem)
-    } else if (show.type === 'cartoon') {
-      cartoonsContainer.insertAdjacentHTML('beforeend', showItem)
+}
+
+const showMovieShow = (arr, type) => {
+  arr.forEach(item => {
+    const showItem = itemTemplate(item)
+
+    if (item.type === type) {
+      const container = document.querySelector(`.${type}sContainer`)
+      container.insertAdjacentHTML('beforeend', showItem)
     }
   });
 }
-showMovieShow(getMovieShow(movieList, 'movie'))
-showMovieShow(getMovieShow(movieList, 'show'))
-showMovieShow(getMovieShow(movieList, 'cartoon'))
+
+['movie', 'show', 'cartoon'].forEach(type => {
+  showMovieShow(getFilterTtems(movieList, type), type)
+})
+
 
 sliderList.forEach((item, i) => {
   setInterval(() => {
@@ -123,7 +123,35 @@ sliderList.forEach((item, i) => {
   }, 4000 * (i + 1));
 })
 
-overlay.addEventListener('click', closeModal)
+const handleSmoothScroll = (event) => {
+  const { currentTarget } = event;
+  const href = currentTarget.getAttribute('href');
+
+  if (!href || !href.startsWith('#') || href.length < 2) return;
+
+  event.preventDefault();
+
+  const element = document.querySelector(href);
+
+  if (!element) return;
+
+  const top = element.offsetTop;
+  window.scrollTo({ behavior: 'smooth', top })
+};
+
+const handelFaqItem = (e) => {
+  const { currentTarget: target } = e
+  target.classList.toggle('opened');
+  const isOpened = target.classList.contains('opened');
+  const height = target.querySelector('.answer-title').clientHeight;
+  const content = target.querySelector('.faq-container__answer');
+
+  content.style.height = `${isOpened ? height : 0}px`
+  console.log(height)
+}
+
 burgerBTN.addEventListener('click', toggleBurgerMenu);
-signinBTN.addEventListener('click', handleSign);
-modalBtn.addEventListener('click', closeModal)
+signinBTN.addEventListener('click', handleSignForm);
+modalBtn.addEventListener('click', toggleModal);
+links.forEach(link => link.addEventListener('click', handleSmoothScroll))
+faqItem.forEach(item => item.addEventListener('click', handelFaqItem));
